ed3fc007e350feb6ee997b4a4ed239343cbafa4f
Tj Holowaychuk <tj@vision-media.ca>
2010-11-17 16:12:06 -0800

    Started lib

--- test/should.test.js
+++ test/should.test.js
@@ -6,8 +6,27 @@
 var should = require('should')
   , assert = require('assert');
 
+function err(fn, msg) {
+  try {
+    fn();
+    assert.fail('expected an error');
+  } catch (err) {
+    assert.equal(msg, err.message);
+  }
+}
+
 module.exports = {
   'test .version': function(){
     assert.match(should.version, /^\d+\.\d+\.\d+$/);
+  },
+  
+  'test assertion': function(){
+    'test'.should.be.a.string;
+  },
+  
+  'test negated assertion': function(){
+    err(function(){
+      'test'.should.not.be.a.string;
+    }, "expected 'test' not to be a string");
   }
-}
+};
