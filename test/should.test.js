9048d5e02779858c7d705544d2731148f0900310
Tj Holowaychuk <tj@vision-media.ca>
2010-11-17 16:40:09 -0800

    Added a()

--- test/should.test.js
+++ test/should.test.js
@@ -24,30 +24,18 @@ module.exports = {
     'test'.should.be.a.string;
   },
   
-  'test negated assertion': function(){
-    err(function(){
-      'test'.should.not.be.a.string;
-    }, "expected 'test' not to be a string");
-  },
-  
-  'test typeof string': function(){
-    'test'.should.be.a.string;
+  'test typeof': function(){
+    'test'.should.be.a('string');
 
     err(function(){
-      'test'.should.not.be.a.string;
+      'test'.should.not.be.a('string');
     }, "expected 'test' not to be a string");
-  },
-  
-  'test typeof number': function(){
-    (5).should.be.a.number;
+    
+    (5).should.be.a('number');
 
     err(function(){
-      (5).should.not.be.a.number;
+      (5).should.not.be.a('number');
     }, "expected 5 not to be a number");
-  
-    err(function(){
-      'test'.should.be.a.number;
-    }, "expected 'test' to be a number");
   },
   
   'test above(n)': function(){
