a65ef7fea1e20af574769d46a84a4f930e4cfbc8
Dominic Tarr <dominic.tarr@gmail.com>
2010-11-29 16:00:49 +1300

    moved should/describe to should.describe

--- test/describe.test.js
+++ test/describe.test.js
@@ -1,6 +1,6 @@
 //describe.test.js
 
-var describe = require('should/describe')
+var describe = require('should').describe
 
 module.exports = {
   'can describe an object': function (){
@@ -10,12 +10,18 @@ module.exports = {
     it.should.eql(1)
     it.should.be.within(0,2)
     
-    
     describe(it.should,"a should about 1")
-      .should.eql((1).should)
+      .should.have.property('obj',1)
   },
   'includes description in error message': function (){
+    try{
     describe(1,"the number 1")
       .should.eql(-1)
+    } catch (e){
+      describe(e,"an error from a failed should")
+        .should.have.property('message')
+      describe(e.message,"message from failed should about 1")
+        .should.include.string("the number 1")
+    }
   }
 }
