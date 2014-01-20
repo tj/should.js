f586fb6a89ffaa3be63767a7188f41585b249822
Thomas Rix <tom@rixth.org>
2014-01-19 23:56:53 -0500

    Rebuilt should.js

--- should.js
+++ should.js
@@ -18,10 +18,11 @@ should
   .use(require('./ext/property'))
   .use(require('./ext/error'))
   .use(require('./ext/match'))
+  .use(require('./ext/jquery'))
   .use(require('./ext/deprecated'));
 
  module.exports = should;
-},{"./ext/assert":3,"./ext/bool":4,"./ext/chain":5,"./ext/deprecated":6,"./ext/eql":7,"./ext/error":8,"./ext/match":9,"./ext/number":10,"./ext/property":11,"./ext/string":12,"./ext/type":13,"./should":14}],2:[function(require,module,exports){
+},{"./ext/assert":3,"./ext/bool":4,"./ext/chain":5,"./ext/deprecated":6,"./ext/eql":7,"./ext/error":8,"./ext/jquery":9,"./ext/match":10,"./ext/number":11,"./ext/property":12,"./ext/string":13,"./ext/type":14,"./should":15}],2:[function(require,module,exports){
 /*!
  * Should
  * Copyright(c) 2010-2014 TJ Holowaychuk <tj@vision-media.ca>
@@ -125,7 +126,7 @@ function objEquiv (a, b) {
   return true;
 }
 
-},{"./util":15}],3:[function(require,module,exports){
+},{"./util":16}],3:[function(require,module,exports){
 /*!
  * Should
  * Copyright(c) 2010-2014 TJ Holowaychuk <tj@vision-media.ca>
@@ -183,7 +184,7 @@ module.exports = function(should) {
     }
   };
 };
-},{"../util":15,"assert":16}],4:[function(require,module,exports){
+},{"../util":16,"assert":17}],4:[function(require,module,exports){
 /*!
  * Should
  * Copyright(c) 2010-2014 TJ Holowaychuk <tj@vision-media.ca>
@@ -258,7 +259,7 @@ module.exports = function(should, Assertion) {
     }));
   });
 };
-},{"../eql":2,"../util":15}],7:[function(require,module,exports){
+},{"../eql":2,"../util":16}],7:[function(require,module,exports){
 /*!
  * Should
  * Copyright(c) 2010-2014 TJ Holowaychuk <tj@vision-media.ca>
@@ -335,6 +336,231 @@ module.exports = function(should, Assertion) {
   Assertion.alias('throw', 'throwError');
 };
 },{}],9:[function(require,module,exports){
+/*!
+ * Should
+ * Copyright(c) 2010-2014 TJ Holowaychuk <tj@vision-media.ca>
+ * MIT Licensed
+ */
+
+ /*!
+  * Portions copyright (c) 2010, 2011, 2012 Wojciech Zawistowski, Travis Jeffery
+  * From the jasmine-jquery project under the MIT License.
+  */
+
+module.exports = function(should, Assertion) {
+  var $ = this.jQuery || this.$;
+
+  /* Otherwise, node's util.inspect loops hangs */
+  if (typeof HTMLElement === 'function' && !HTMLElement.prototype.inspect) {
+    HTMLElement.prototype.inspect = function () {
+      return this.outerHTML;
+    };
+  }
+
+  jQuery.fn.inspect = function () {
+    var elementList = this.toArray().map(function (e) {
+      return e.inspect();
+    }).join(", ");
+    if (this.selector) {
+      return "SELECTOR(" + this.selector + ") matching " + this.length + " elements" + (elementList.length ? ": " + elementList : "");
+    } else {
+      return elementList;
+    }
+  }
+
+  var hasProperty = function (actualValue, expectedValue) {
+    if (expectedValue === undefined) {
+      return actualValue !== undefined;
+    }
+
+    return actualValue === expectedValue;
+  };
+
+  var browserTagCaseIndependentHtml = function (html) {
+    return $('<div/>').append(html).html();
+  };
+
+  Assertion.add('haveClass', function(className) {
+    this.params = { operator: 'to have class ' + className };
+    this.assert($(this.obj).hasClass(className));
+  });
+
+  Assertion.add('haveCss', function(css) {
+    this.params = { operator: 'to have css ' + JSON.stringify(css) };
+    for (var prop in css) {
+      var value = css[prop];
+      if (value === 'auto' && $(this.obj).get(0).style[prop] === 'auto') {
+        continue;
+      }
+      if ($(this.obj).css(prop) !== value) {
+        this.assert(false);
+      }
+    }
+    this.assert(true);
+  });
+
+  Assertion.add('visible', function() {
+    this.params = { operator: 'to be visible' };
+    this.assert($(this.obj).is(':visible'));
+  }, true);
+
+  Assertion.add('hidden', function() {
+    this.params = { operator: 'to be hidden' };
+    this.assert($(this.obj).is(':hidden'));
+  }, true);
+
+  Assertion.add('selected', function() {
+    this.params = { operator: 'to be selected' };
+    this.assert($(this.obj).is(':selected'));
+  }, true);
+
+  Assertion.add('checked', function() {
+    this.params = { operator: 'to be checked' };
+    this.assert($(this.obj).is(':checked'));
+  }, true);
+
+  Assertion.add('emptyJq', function() {
+    this.params = { operator: 'to be empty' };
+    this.assert($(this.obj).is(':empty'));
+  }, true);
+
+  Assertion.add('inDOM', function() {
+    this.params = { operator: 'to be in the DOM' };
+    this.assert($.contains(document.documentElement, $(this.obj)[0]));
+  }, true);
+
+  Assertion.add('exist', function() {
+    this.params = { operator: 'to exist' };
+    this.assert($(this.obj).length);
+  }, true);
+
+  Assertion.add('haveLength', function(length) {
+    this.params = { operator: 'to have length ' + length };
+    this.assert($(this.obj).length === length);
+  });
+
+  Assertion.add('haveAttr', function(attributeName, expectedAttributeValue) {
+    this.params = { operator: 'to have attribute ' + attributeName + ' with value ' + expectedAttributeValue };
+    this.assert(hasProperty($(this.obj).attr(attributeName), expectedAttributeValue));
+  });
+
+  Assertion.add('haveProp', function(propertyName, expectedPropertyValue) {
+    this.params = { operator: 'to have property ' + propertyName + ' with value ' + expectedPropertyValue };
+    this.assert(hasProperty($(this.obj).prop(propertyName), expectedPropertyValue));
+  });
+
+  Assertion.add('haveId', function(id) {
+    this.params = { operator: 'to have ID ' + id };
+    this.assert($(this.obj).attr('id') === id);
+  });
+
+  Assertion.add('haveHtml', function(html) {
+    this.params = { operator: 'to have HTML ' + html };
+    this.assert($(this.obj).html() == browserTagCaseIndependentHtml(html));
+  });
+
+  Assertion.add('containHtml', function(html) {
+    this.params = { operator: 'to contain HTML ' + html };
+    this.assert($(this.obj).html().indexOf(browserTagCaseIndependentHtml(html)) >= 0);
+  });
+
+  Assertion.add('haveText', function(text) {
+    this.params = { operator: 'to have text ' + text };
+    var trimmedText = $.trim($(this.obj).text());
+
+    if (text && $.isFunction(text.test)) {
+      this.assert(text.test(trimmedText));
+    } else {
+      this.assert(trimmedText === text);
+    }
+  });
+
+  Assertion.add('containText', function(text) {
+    this.params = { operator: 'to contain text ' + text };
+    var trimmedText = $.trim($(this.obj).text());
+
+    if (text && $.isFunction(text.test)) {
+      this.assert(text.test(trimmedText));
+    } else {
+      this.assert(trimmedText.indexOf(text) !== -1);
+    }
+  });
+
+  Assertion.add('haveValue', function(val) {
+    this.params = { operator: 'to have value ' + val };
+    this.assert($(this.obj).val() === val);
+  });
+
+  Assertion.add('haveData', function(key, expectedValue) {
+    this.params = { operator: 'to have data ' + key + ' with value ' + expectedValue };
+    this.assert(hasProperty($(this.obj).data(key), expectedValue));
+  });
+
+  Assertion.add('containElement', function(target) {
+    this.params = { operator: 'to contain ' + $(target).inspect() };
+    this.assert($(this.obj).find(target).length);
+  });
+
+  Assertion.add('matchedBy', function(selector) {
+    this.params = { operator: 'to be matched by selector ' + selector };
+    this.assert($(this.obj).filter(selector).length);
+  });
+
+  Assertion.add('disabled', function() {
+    this.params = { operator: 'to be disabled' };
+    this.assert($(this.obj).is(':disabled'));
+  }, true);
+
+  Assertion.add('focused', function() {
+    this.params = { operator: 'to be focused' };
+    this.assert($(this.obj)[0] === $(this.obj)[0].ownerDocument.activeElement);
+  }, true);
+
+  Assertion.add('handle', function(event) {
+    this.params = { operator: 'to handle ' + event };
+
+    var events = $._data($(this.obj).get(0), "events");
+
+    if (!events || !event || typeof event !== "string") {
+      return this.assert(false);
+    }
+
+    var namespaces = event.split("."),
+        eventType = namespaces.shift(),
+        sortedNamespaces = namespaces.slice(0).sort(),
+        namespaceRegExp = new RegExp("(^|\\.)" + sortedNamespaces.join("\\.(?:.*\\.)?") + "(\\.|$)");
+
+    if (events[eventType] && namespaces.length) {
+      for (var i = 0; i < events[eventType].length; i++) {
+        var namespace = events[eventType][i].namespace;
+
+        if (namespaceRegExp.test(namespace)) {
+          return this.assert(true);
+        }
+      }
+    } else {
+      return this.assert(events[eventType] && events[eventType].length > 0);
+    }
+
+    return this.assert(false);
+  });
+
+  Assertion.add('handleWith', function(eventName, eventHandler) {
+    this.params = { operator: 'to handle ' + eventName + ' with ' + eventHandler };
+
+    var normalizedEventName = eventName.split('.')[0],
+        stack = $._data($(this.obj).get(0), "events")[normalizedEventName];
+
+    for (var i = 0; i < stack.length; i++) {
+      if (stack[i].handler == eventHandler) {
+        return this.assert(true);
+      }
+    }
+
+    return this.assert(false);
+  });
+};
+},{}],10:[function(require,module,exports){
 /*!
  * Should
  * Copyright(c) 2010-2014 TJ Holowaychuk <tj@vision-media.ca>
@@ -447,7 +673,7 @@ module.exports = function(should, Assertion) {
     }, this);
   });
 };
-},{"../eql":2,"../util":15}],10:[function(require,module,exports){
+},{"../eql":2,"../util":16}],11:[function(require,module,exports){
 /*!
  * Should
  * Copyright(c) 2010-2014 TJ Holowaychuk <tj@vision-media.ca>
@@ -498,7 +724,7 @@ module.exports = function(should, Assertion) {
   Assertion.alias('below', 'lessThan');
 
 };
-},{}],11:[function(require,module,exports){
+},{}],12:[function(require,module,exports){
 /*!
  * Should
  * Copyright(c) 2010-2014 TJ Holowaychuk <tj@vision-media.ca>
@@ -714,7 +940,7 @@ module.exports = function(should, Assertion) {
   });
 
 };
-},{"../eql":2,"../util":15}],12:[function(require,module,exports){
+},{"../eql":2,"../util":16}],13:[function(require,module,exports){
 /*!
  * Should
  * Copyright(c) 2010-2014 TJ Holowaychuk <tj@vision-media.ca>
@@ -734,7 +960,7 @@ module.exports = function(should, Assertion) {
     this.assert(this.obj.indexOf(str, this.obj.length - str.length) >= 0);
   });
 };
-},{}],13:[function(require,module,exports){
+},{}],14:[function(require,module,exports){
 /*!
  * Should
  * Copyright(c) 2010-2014 TJ Holowaychuk <tj@vision-media.ca>
@@ -812,7 +1038,7 @@ module.exports = function(should, Assertion) {
 
   Assertion.alias('instanceof', 'instanceOf');
 };
-},{"../util":15}],14:[function(require,module,exports){
+},{"../util":16}],15:[function(require,module,exports){
 /*!
  * Should
  * Copyright(c) 2010-2014 TJ Holowaychuk <tj@vision-media.ca>
@@ -983,7 +1209,7 @@ Assertion.prototype = {
 };
 
 
-},{"./util":15}],15:[function(require,module,exports){
+},{"./util":16}],16:[function(require,module,exports){
 /*!
  * Should
  * Copyright(c) 2010-2014 TJ Holowaychuk <tj@vision-media.ca>
@@ -1103,7 +1329,7 @@ exports.forOwn = function(obj, f, context) {
     }
   }
 };
-},{"assert":16,"util":19}],16:[function(require,module,exports){
+},{"assert":17,"util":20}],17:[function(require,module,exports){
 // http://wiki.commonjs.org/wiki/Unit_Testing/1.0
 //
 // THIS IS NOT TESTED NOR LIKELY TO WORK OUTSIDE V8!
@@ -1465,7 +1691,7 @@ var objectKeys = Object.keys || function (obj) {
   return keys;
 };
 
-},{"util/":19}],17:[function(require,module,exports){
+},{"util/":20}],18:[function(require,module,exports){
 if (typeof Object.create === 'function') {
   // implementation from standard node.js 'util' module
   module.exports = function inherits(ctor, superCtor) {
@@ -1490,14 +1716,14 @@ if (typeof Object.create === 'function') {
   }
 }
 
-},{}],18:[function(require,module,exports){
+},{}],19:[function(require,module,exports){
 module.exports = function isBuffer(arg) {
   return arg && typeof arg === 'object'
     && typeof arg.copy === 'function'
     && typeof arg.fill === 'function'
     && typeof arg.readUInt8 === 'function';
 }
-},{}],19:[function(require,module,exports){
+},{}],20:[function(require,module,exports){
 // Copyright Joyent, Inc. and other Node contributors.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a
@@ -2085,6 +2311,6 @@ function hasOwnProperty(obj, prop) {
   return Object.prototype.hasOwnProperty.call(obj, prop);
 }
 
-},{"./support/isBuffer":18,"inherits":17}]},{},[1])
+},{"./support/isBuffer":19,"inherits":18}]},{},[1])
 (1)
 });
